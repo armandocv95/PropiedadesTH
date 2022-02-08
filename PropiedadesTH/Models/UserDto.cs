9132b819f843f2c37c7ee9664ef762ae0bc06bcb
Armando Castillo Vazquez <acastillo5@uabc.edu.mx>
2022-02-07 21:11:34 -0800

    implementacion de seguridad

---
+++ PropiedadesTH/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PropiedadesTH.Models
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
